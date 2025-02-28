c83035b1c189803f40c2c663a7312bff81fdad65
tlkhanh <longkhanhta@gmail.com>
2025-03-01 00:00:01 +0700

    Add project files.

---
+++ net-task/NetTask.cs
@@ -0,0 +1,59 @@
+﻿namespace net_task;
+internal class NetTask
+{
+    private readonly object _lock = new();
+
+    private bool _completed;
+    private Exception? _exception;
+    public bool IsCompleted
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _completed;
+            }
+        }
+    }
+
+    public static NetTask Run(Action action)
+    {
+        var task = new NetTask();
+        try
+        {
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                try
+                {
+                    action();
+                    task.SetResult();
+                }
+                catch (Exception ex)
+                {
+                    task.SetException(ex);
+                }
+            });
+        }
+        catch (Exception ex)
+        {
+
+        }
+        return task;
+    }
+
+    public void SetException(Exception ex) => CompleteTask(ex);
+    public void SetResult() => CompleteTask();
+
+    public void CompleteTask(Exception? exception = null)
+    {
+        lock (_lock)
+        {
+            if (_completed)
+            {
+                throw new InvalidOperationException("Task already completed");
+            }
+            _completed = true;
+            _exception = exception;
+        }
+    }
+}
---
+++ net-task/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using net_task;
+
+Console.WriteLine($"Start thread id: {Environment.CurrentManagedThreadId}");
+
+NetTask.Run(() => Console.WriteLine($"First NetTask thread id: {Environment.CurrentManagedThreadId}"));
+NetTask.Run(() => Console.WriteLine($"Second NetTask thread id: {Environment.CurrentManagedThreadId}"));
+
+
+Console.ReadLine();
